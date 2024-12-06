10d87d58fe7a99253d5e3d5c4c03a8858dbd2df3
Ahamed Fofana <ahamedfofana12@gmail.com>
2024-12-06 07:14:31 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
+void invalidCommand(std::string input){
+  std::cout << "$ ";
+  std::getline(std::cin, input);
+  std::cout << input << ": command not found" << std::endl;
+
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
+  // Uncomment this block to pass the first stage  
   std::string input;
-  std::getline(std::cin, input);
-  
-  std::cout << input << ": command not found" << std::endl;
+  while(true){
+    invalidCommand(input);
+  }
+  return 0;
 }
